c5c452c89444ac01bbc67154b24b53cd380ef443
gdgim-gif <gdgim@ou.ac.lk>
2026-07-23 11:41:05 +0530

    Third eddtion

--- student.java
+++ student.java
@@ -14,6 +14,10 @@
         System.out.println("Index No: GSCOMP384");
         System.out.println("Registrtion: ICTS/76");
         System.out.println("Game -IGI");
+        System.out.println("Gamage");
+        System.out.println("Chandar");
+        System.out.println("My Divya");
+
 
     }
 }
