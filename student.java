4a279bf66db70373b46d4147f4b2ab457fcd7a36
gdgim-gif <gdgim@ou.ac.lk>
2026-07-23 11:37:35 +0530

    Second Change Adding Another Data

--- student.java
+++ student.java
@@ -4,10 +4,16 @@
     // Index No: GSCOMP384
     //UNi - Jaffna
 
+    //School - sen/sebasthiyan national collage
+    //Grade 12A
+
     public static void main(String[] args) {
 
         System.out.println("Name: Supun Garusinghe");
         System.out.println("Index No: GSCOMP384");
+        System.out.println("Index No: GSCOMP384");
+        System.out.println("Registrtion: ICTS/76");
+        System.out.println("Game -IGI");
 
     }
 }
