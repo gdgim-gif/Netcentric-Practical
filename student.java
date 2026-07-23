8b090f6c3d71370ea68a30418459f84e6cf09e7e
gdgim-gif <gdgim@ou.ac.lk>
2026-07-23 11:27:57 +0530

    Newly Created File

---
+++ student.java
@@ -0,0 +1,13 @@
+ class Student {
+
+    // Name: Supun Garusinghe
+    // Index No: GSCOMP384
+    //UNi - Jaffna
+
+    public static void main(String[] args) {
+
+        System.out.println("Name: Supun Garusinghe");
+        System.out.println("Index No: GSCOMP384");
+
+    }
+}
